bbd0b57c025edce16a2c9d3739b30bb81aee93c6
T14s <122837733+looonnng@users.noreply.github.com>
2024-09-26 23:28:26 -0500

    feat: create function to display current temp
    could refactor this later

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -19,4 +19,38 @@ export async function displayCurrentWeatherIcon(jsonData) {
   iconWrapper.appendChild(icon);
 }
 
-export default { displayCurrentWeatherIcon };
+export function displayCurrentTemp(jsonData) {
+  const currentTemp = jsonData.currentConditions.temp;
+  const currentTempWrapper = document.querySelector(
+    '.current-temperature-wrapper',
+  );
+
+  const weatherText = document.createElement('div');
+  const weatherUnits = document.createElement('div');
+  const fahrenheit = document.createElement('div');
+  const celsius = document.createElement('div');
+  const fahrenheitLink = document.createElement('a');
+  const celsiusLink = document.createElement('a');
+
+  weatherText.className = 'current-temperature flex';
+  weatherUnits.className = 'weather-units flex';
+  fahrenheit.className = 'fahrenheit';
+  celsius.className = 'celsius';
+  fahrenheitLink.className = 'fahrenheit-link';
+  celsiusLink.className = 'celsius-link';
+
+  fahrenheitLink.textContent = 'F°';
+  celsiusLink.textContent = 'C°';
+  weatherText.textContent = Math.ceil(currentTemp);
+
+  fahrenheitLink.href = '#';
+  celsiusLink.href = '#';
+
+  fahrenheit.appendChild(fahrenheitLink);
+  celsius.appendChild(celsiusLink);
+  weatherUnits.append(fahrenheit, celsius);
+  weatherText.appendChild(weatherUnits);
+  currentTempWrapper.appendChild(weatherText);
+}
+
+export default { displayCurrentWeatherIcon, displayCurrentTemp };
