2399f3505ad2bf4b44b85b37b819dab1c1520fff
T14s <122837733+looonnng@users.noreply.github.com>
2024-10-02 22:38:51 -0500

    build: update entry

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/modules/utils.js',
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
     static: './dist',
